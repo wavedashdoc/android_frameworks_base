5316f6d2fc09fe1f54a24f649b8c78a866656916
ezio84 <brabus84@gmail.com>
2018-06-22 17:28:16 +0200

    Add dynamic Immersive Mode tile
    on top of existing aosp policy control code
    
    - immersive statusbar
    - immersive navbar
    - full immersive
    
    quicksearchbox apk is blacklisted to avoid battery drain / unlock lag
    
    - tap to cycle modes
    - long press to enable the selected immersive mode
    - if the current immersive mode is enabled, long pressing again the tile
    will disable immersive so no need to cycle the tile to go to the "off"
    position
    - tile icon will be set to active or unactive state according to the
    current mode
    
    Change-Id: I1747b7f4077247014ca5e2a160712354df6e023e
    
    Immersive tile: more cleanup
    
    no need to have a specific "off" state because we already allow to
    disable immersive longpressing on one of the other states when
    they are active
    
    Change-Id: I524a8927a1e9f89244fccefabc5dd1926fed6608
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -40,6 +40,7 @@ import com.android.systemui.qs.tiles.DndTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.HWKeysTile;
+import com.android.systemui.qs.tiles.ImmersiveTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocaleTile;
 import com.android.systemui.qs.tiles.LocationTile;
@@ -93,6 +94,7 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<ScreenrecordTile> mScrTileProvider;
     private final Provider<LocaleTile> mLocaleTileProvider;
     private final Provider<AlwaysOnDisplayTile> mAlwaysOnDisplayTileProvider;
+    private final Provider<ImmersiveTile> mImmersiveTileProvider;
 
     private QSTileHost mHost;
 
@@ -123,7 +125,8 @@ public class QSFactoryImpl implements QSFactory {
             Provider<HWKeysTile> hWKeysTileProvider,
             Provider<ScreenrecordTile> scrTileProvider,
             Provider<LocaleTile> localeTileProvider,
-            Provider<AlwaysOnDisplayTile> alwaysOnDisplayTileProvider) {
+            Provider<AlwaysOnDisplayTile> alwaysOnDisplayTileProvider,
+            Provider<ImmersiveTile> immersiveTileProvider) {
         mWifiTileProvider = wifiTileProvider;
         mBluetoothTileProvider = bluetoothTileProvider;
         mCellularTileProvider = cellularTileProvider;
@@ -151,6 +154,7 @@ public class QSFactoryImpl implements QSFactory {
         mScrTileProvider = scrTileProvider;
         mLocaleTileProvider = localeTileProvider;
         mAlwaysOnDisplayTileProvider = alwaysOnDisplayTileProvider;
+        mImmersiveTileProvider = immersiveTileProvider;
     }
 
     public void setHost(QSTileHost host) {
@@ -224,6 +228,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mAlwaysOnDisplayTileProvider.get();
             case "theme":
                 return new ThemeTile(mHost);
+            case "immersive":
+                return mImmersiveTileProvider.get();
         }
 
         // Intent tiles.
