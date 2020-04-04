8c3d8ea71766b78d738859ad2319992f017912b7
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-04-04 13:12:10 -0300

    SystemUI: Keyguard: Only apply padding if fod view showing
    Change-Id: I3d2a5b0e861c746887db88f5bcc6b6a35e6e1570
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
@@ -73,6 +73,7 @@ import com.android.systemui.Dependency;
 import com.android.systemui.Interpolators;
 import com.android.systemui.R;
 import com.android.systemui.assist.AssistManager;
+import com.android.systemui.biometrics.FODCircleView;
 import com.android.systemui.plugins.ActivityStarter;
 import com.android.systemui.plugins.IntentButtonProvider;
 import com.android.systemui.plugins.IntentButtonProvider.IntentButton;
@@ -118,6 +119,8 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
     protected static final Intent PHONE_INTENT = new Intent(Intent.ACTION_DIAL);
     private static final int DOZE_ANIMATION_STAGGER_DELAY = 48;
     private static final int DOZE_ANIMATION_ELEMENT_DURATION = 250;
+
+    private static final String FOD = "vendor.lineage.biometrics.fingerprint.inscreen";
 
     private KeyguardAffordanceView mRightAffordanceView;
     private KeyguardAffordanceView mLeftAffordanceView;
@@ -429,8 +432,8 @@ public class KeyguardBottomAreaView extends FrameLayout implements View.OnClickL
     }
 
     private boolean hasInDisplayFingerprint() {
-        return mContext.getResources().getBoolean(
-               com.android.internal.R.bool.config_needCustomFODView)  && mIsFingerprintRunning;
+        return mContext.getPackageManager().hasSystemFeature(FOD) && mIsFingerprintRunning &&
+                   FODCircleView.canUnlockWithFp(KeyguardUpdateMonitor.getInstance(mContext));
     }
 
     public boolean isLeftVoiceAssist() {
