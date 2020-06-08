1f4543e577a5b47de574b9a2e2207b496b429004
Ezio Lacandia Bijelkic <brabus84@gmail.com>
2020-06-09 01:16:44 +0200

    Ambient music / Now playing: show also on lockscreen now that we have now playing support (and eventually alternative client support next days) it's handy to get track info on lockscreen too, because now playing notification is not visible on lockscreen by default.
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: I87e972bb5145612d62e4ee3bbade91a7502f929a

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PanelView.java
@@ -1125,8 +1125,8 @@ public abstract class PanelView extends FrameLayout {
         setAnimator(animator);
 
         View[] viewsToAnimate = {
-                mKeyguardBottomArea.getIndicationArea()/*,
-                mStatusBar.getAmbientIndicationContainer()*/};
+                mKeyguardBottomArea.getIndicationArea(),
+                mStatusBar.getAmbientIndicationContainer()};
         for (View v : viewsToAnimate) {
             if (v == null) {
                 continue;
