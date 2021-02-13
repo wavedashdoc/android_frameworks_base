18e0e12b7a07063be16138d847cde0688c0f4faf
wavedashdoc <31520009+wavedashdoc@users.noreply.github.com>
2021-02-12 20:42:24 -0500

    Update CachedBluetoothDevice.java
    Fix build error: frameworks/base/packages/SettingsLib/src/com/android/settingslib/bluetooth/Cache
    dBluetoothDevice.java:807: error: ')' expected
                            == BluetoothClass.Device.AUDIO_VIDEO_WEARABLE_HEADSET) {
                                                                                  ^

--- packages/SettingsLib/src/com/android/settingslib/bluetooth/CachedBluetoothDevice.java
+++ packages/SettingsLib/src/com/android/settingslib/bluetooth/CachedBluetoothDevice.java
@@ -794,14 +794,12 @@ public class CachedBluetoothDevice implements Comparable<CachedBluetoothDevice> 
 
     private void processPhonebookAccess() {
         if (mDevice.getBondState() != BluetoothDevice.BOND_BONDED) return;
-
         ParcelUuid[] uuids = mDevice.getUuids();
         if (BluetoothUuid.containsAnyUuid(uuids, PbapServerProfile.PBAB_CLIENT_UUIDS)) {
             // The pairing dialog now warns of phone-book access for paired devices.
             // No separate prompt is displayed after pairing.
             if (mDevice.getPhonebookAccessPermission() == BluetoothDevice.ACCESS_UNKNOWN) {
-                if ((mDevice.getBluetoothClass() != null) &&
-                    (mDevice.getBluetoothClass().getDeviceClass()
+                if (mDevice.getBluetoothClass().getDeviceClass()
                         == BluetoothClass.Device.AUDIO_VIDEO_HANDSFREE ||
                     mDevice.getBluetoothClass().getDeviceClass()
                         == BluetoothClass.Device.AUDIO_VIDEO_WEARABLE_HEADSET) {
